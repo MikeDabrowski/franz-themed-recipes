dd5740fccea678daf411e92f19001dc38daf7a37
Michał Dąbrowski <mdabrowski@pretius.com>
2018-12-11 14:10:02 +0100

    fix(Gmail): Improved email badge

--- gmail/webview.js
+++ gmail/webview.js
@@ -14,6 +14,23 @@ module.exports = Franz => {
       count = parseInt(document.querySelector('[role="navigation"] .bsU').innerHTML, 10);
     }
 
+    if (document.getElementsByClassName('J-Ke n0').length > 0) {
+      // 2nd best (more detailed check, much more accurate if available)
+      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label') != null) {
+        count = parseInt(document.getElementsByClassName('J-Ke n0')[0].getAttribute('aria-label').replace(/[^0-9.]/g, ''), 10);
+      }
+
+      // 1st best
+      if (document.getElementsByClassName('J-Ke n0')[0].getAttribute('title') != null) {
+        count = parseInt(document.getElementsByClassName('J-Ke n0')[0].getAttribute('title').replace(/[^0-9.]/g, ''), 10);
+      }
+    }
+
+    // Just incase we don't end up with a number, set it back to zero (parseInt can return NaN)
+    if (isNaN(count)) {
+      count = 0;
+    }
+
     Franz.setBadge(count);
   };
 

ecc613c2b051d4a648972513aa6dbbd267eb0efd
MikeDabrowski <mike2060@gmail.com>
2019-02-06 17:34:53 +0100

    fix(Messenger): Fix loading theme

--- messenger/webview.js
+++ messenger/webview.js
@@ -4,6 +4,11 @@ const fs = require('fs');
 const path = require('path');
 
 module.exports = Franz => {
+  const cssFiles = fs.readdirSync(__dirname)
+    .filter((fileName) => (fileName.startsWith('theme-') && fileName.endsWith('.css')))
+    .map((fileName) => path.join(__dirname, fileName));
+  Franz.injectCSS(...cssFiles);
+
   const getMessages = function getMessages() {
     let count = document.querySelectorAll('._5fx8:not(._569x),._1ht3:not(._569x)').length;
     const messageRequestsElement = document.querySelector('._5nxf');

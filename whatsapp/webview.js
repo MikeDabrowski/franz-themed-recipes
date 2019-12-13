aec220b3044aa803022f6e6d193338ee1445336d
MikeDabrowski <mike2060@gmail.com>
2019-12-13 10:38:50 +0100

    update(WhatsApp): Adapt new changes

--- whatsapp/webview.js
+++ whatsapp/webview.js
@@ -12,7 +12,9 @@ const {
   session
 } = webContents;
 setTimeout(() => {
-  if (document.querySelector('body').innerHTML.includes('Google Chrome 36+')) {
+  const elem = document.querySelector('.landing-title.version-title');
+
+  if (elem && elem.innerText.toLowerCase().includes('google chrome')) {
     window.location.reload();
   }
 }, 1000);
@@ -34,11 +36,11 @@ window.addEventListener('beforeunload', async () => {
 
 module.exports = Franz => {
   const getMessages = function getMessages() {
-    const elements = document.querySelectorAll('.CxUIE, .unread');
+    const elements = document.querySelectorAll('.CxUIE, .unread, ._0LqQ');
     let count = 0;
 
     for (let i = 0; i < elements.length; i += 1) {
-      if (elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
+      if (elements[i].querySelectorAll('.P6z4j').length === 1 && elements[i].querySelectorAll('*[data-icon="muted"]').length === 0) {
         count += 1;
       }
     }
